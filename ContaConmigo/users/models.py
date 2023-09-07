18cf2dd63e6d9c726a9a9fb6ad13838ffe736489
LianaCarlaPassaro <lianapassaro@hotmail.com>
2023-09-07 15:45:41 -0300

    agregarcolumna

--- ContaConmigo/users/models.py
+++ ContaConmigo/users/models.py
@@ -109,7 +109,7 @@ class Donantes(models.Model):
     comentario = models.TextField(max_length=1024, null=True, blank=True)
     confirmacionAsistencia = models.BooleanField(default=False)
     pacienteInstitucion = models.ManyToManyField(PacienteInstitucion, related_name="donantePacienteInstitucion")
-    
+    agradecimiento = models.TextField(max_length=1024, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True, null=True,verbose_name="created at")
     updated_at = models.DateTimeField(auto_now=True,null=True,verbose_name="updated at")
 
